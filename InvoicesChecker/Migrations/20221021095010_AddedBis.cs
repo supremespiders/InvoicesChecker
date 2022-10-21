c9b988c2272e59fca5fcc4861fbc3ac7854c99c6
riadh chebbi <supremespiders@gmail.com>
2022-10-21 11:31:14 +0100

    migration missed

---
+++ InvoicesChecker/Migrations/20221021095010_AddedBis.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoicesChecker.Migrations
+{
+    public partial class AddedBis : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBis",
+                table: "Payments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBis",
+                table: "Payments");
+        }
+    }
+}
